2a074783e92e70466eaff7c5718f45b82727a347
Blak3Nick <blaken2021@gmail.com>
2018-11-08 12:40:50 -0500

    added new dependencies

--- web/src/main/java/academy/learnprograming/WebMain.java
+++ web/src/main/java/academy/learnprograming/WebMain.java
@@ -1,4 +1,14 @@
 package academy.learnprograming;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
 public class WebMain {
+    public static void main(String[] args) {
+        log.info("running the main class");
+        SpringApplication.run(WebMain.class, args);
+    }
 }
