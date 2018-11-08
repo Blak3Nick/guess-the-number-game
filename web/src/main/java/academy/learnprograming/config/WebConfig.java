9a4444f2a8f90ae49674074f6b0da80db4bac873
Blak3Nick <blaken2021@gmail.com>
2018-11-08 12:56:07 -0500

    added a home page

---
+++ web/src/main/java/academy/learnprograming/config/WebConfig.java
@@ -0,0 +1,14 @@
+package academy.learnprograming.config;
+
+import academy.learnprograming.util.ViewNames;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+}
---
+++ web/src/main/java/academy/learnprograming/util/ViewNames.java
@@ -0,0 +1,10 @@
+package academy.learnprograming.util;
+
+public final class ViewNames {
+
+    public static final String HOME = "home";
+
+    private ViewNames() {
+
+    }
+}
