88291b0d9bed1acbb4f556d0a1d3d9698bd0638e
Blak3Nick <blaken2021@gmail.com>
2018-11-09 12:58:03 -0500

    added devtools spring dependency

---
+++ web/src/main/java/academy/learnprograming/service/GameService.java
@@ -0,0 +1,11 @@
+package academy.learnprograming.service;
+
+public interface GameService {
+
+    boolean isGameOver();
+    String getMainMessage();
+    String getResultMessage();
+    void checkGuess(int guess);
+    void reset();
+
+}
