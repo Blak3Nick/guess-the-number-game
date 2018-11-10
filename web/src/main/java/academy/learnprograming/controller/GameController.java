71e25fcc79fcf2749d7de8d3726995fc2cee5b09
Blak3Nick <blaken2021@gmail.com>
2018-11-10 05:58:24 -0500

    added devtools spring dependency

--- web/src/main/java/academy/learnprograming/controller/GameController.java
+++ web/src/main/java/academy/learnprograming/controller/GameController.java
@@ -1,9 +1,14 @@
 package academy.learnprograming.controller;
 
 import academy.learnprograming.service.GameService;
+import academy.learnprograming.util.AttributeNames;
+import academy.learnprograming.util.GameMappings;
+import academy.learnprograming.util.ViewNames;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
@@ -14,6 +19,14 @@ public class GameController {
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
-
-
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model){
+        model.addAttribute(AttributeNames.MAIN_MESSAGE,gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE,gameService.getResultMessage());
+        log.info("model= {}", model);
+        if (gameService.isGameOver()) {
+            return ViewNames.GAME_OVER;
+        }
+        return ViewNames.PLAY;
+    }
 }
