26975f9252042b025f2599832bd2ff63eb19facb
Blak3Nick <blaken2021@gmail.com>
2018-11-10 06:30:13 -0500

    fixed a error with @ComponentSCan

--- web/src/main/java/academy/learnprograming/WebMain.java
+++ web/src/main/java/academy/learnprograming/WebMain.java
@@ -3,9 +3,11 @@ package academy.learnprograming;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan(basePackages = "academy.learnprogramming")
 public class WebMain {
     public static void main(String[] args) {
         log.info("running the main class");
